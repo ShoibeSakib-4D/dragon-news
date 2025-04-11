8d195eeadfe040ad3eb2a945626998842bced94b
shoibesakib <shoibesakib81@gmail.com>
2025-04-11 09:46:11 +0600

    Project Done

--- src/components/FindUs.jsx
+++ src/components/FindUs.jsx
@@ -6,9 +6,9 @@ const FindUs = () => {
         <h1 className="font-semibold mb-2">Find Us On</h1>
             <div className="join flex join-vertical">
                 
-  <button className="btn join-item bg-base-100">Button</button>
-  <button className="btn join-item bg-base-100">Button</button>
-  <button className="btn join-item bg-base-100">Button</button>
+  <button className="btn join-item bg-base-100">Facebook</button>
+  <button className="btn join-item bg-base-100">X</button>
+  <button className="btn join-item bg-base-100">instagram</button>
 </div>
         </div>
     );
